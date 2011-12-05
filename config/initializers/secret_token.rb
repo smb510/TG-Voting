ede355afc5881b45bc18bf47a627107931d7a414
Scottie <smb510@gmail.com>
2011-12-04 20:21:58 -0500

    new voting app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OTElections::Application.config.secret_token = 'd3f5b87610f85ecd86d33912b6701bb19ee96833541e063946554b06e8d0a2a5e959f16c2efc96efc60ae4b3b83da53b60e4c37b1e66068bf633511bf4189b21'
