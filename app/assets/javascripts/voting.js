6af53be7ea8df5747d675867513783e388aedc15
Scottie <smb510@gmail.com>
2012-01-31 12:51:23 -0500

    syntax error fix

--- app/assets/javascripts/voting.js
+++ app/assets/javascripts/voting.js
@@ -1,7 +1,18 @@
 $(document).ready(function()
 {
 	
-	
+	$(".show-hide").click(function()
+       {
+           if($(".detailed-results").hasClass("hidden"))
+           {
+               $(".detailed-results").removeClass("hidden");
+           }
+           else
+           {
+               $(".detailed-results").addClass("hidden");
+           }   
+       });
+       
 	$('form').submit(function(event)
 	{
 		var MAX_YES = 14;
