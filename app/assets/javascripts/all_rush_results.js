92ebb22d3d83bcacd9042b5f06b51c3077d87057
Scottie <smb510@gmail.com>
2012-01-31 12:38:18 -0500

    show/hide results added

---
+++ app/assets/javascripts/all_rush_results.js
@@ -0,0 +1,14 @@
+$(document).ready(function()
+{
+    $(".show-hide").click(function())
+    {
+        if($(".detailed-results").hasClass("hidden"))
+        {
+            $(".detailed-results").removeClass("hidden");
+        }
+        else
+        {
+            $(".detailed-results").addClass("hidden");
+    }   
+    }
+});
